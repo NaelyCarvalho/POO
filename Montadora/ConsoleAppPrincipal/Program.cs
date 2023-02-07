20b8ebe29630eea76e4cc4834a149cdd4564ba57
Naely <123516580+NaelyCarvalho@users.noreply.github.com>
2023-02-06 22:00:08 -0300

    atualizando
    atualizando

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -7,18 +7,42 @@ namespace ConsoleAppPrincipal
     {
         static void Main(string[] args)
         {
-            Pneu pneu1 = new Pneu( 16, "Carro de passeio", "Firestone", false, 150);
+            // Pneu pneu1 = new Pneu( 16, "Carro de passeio", "Firestone", false, 150);
 
-            pneu1.Girar(5);
-            pneu1.Exibir();
+            // Pneu pneu2 = new Pneu(16, "Pneu de estepe", "Firestone", true, 70);
 
             Console.WriteLine();
 
-            Pneu pneu2 = new Pneu(16, "Pneu de estepe", "Firestone", true, 70);
+            Carro BMW = new Carro("BMW", "BMW I3", "ABC1J23", 2014, 120);
 
-            pneu2.Girar(5);
-            pneu2.Exibir();
 
+            BMW.Abastecer(80);
+            BMW.Ligar();
+            BMW.Acelerar(15);
+            BMW.Acelerar(5);
+            BMW.Acelerar(22);
+            BMW.Frear(8);
+            BMW.Acelerar(3);
+            BMW.Frear(4);
+            BMW.Desligar();
+            BMW.PneuDianteiroEsquerdo = BMW.PneuEstepe;
+            BMW.Ligar();
+            BMW.Acelerar(15);
+            BMW.Acelerar(5);
+            BMW.Acelerar(22);
+            BMW.Frear(8);
+            BMW.Acelerar(3);
+            BMW.Acelerar(3);
+            BMW.Acelerar(3);
+            BMW.Acelerar(3);
+            BMW.Frear(4);
+            BMW.Abastecer(80);
+            BMW.Ligar();
+            BMW.Acelerar(40);
+            BMW.Acelerar(29);
+            BMW.Acelerar(1);
+            BMW.Acelerar(1);
+            BMW.Exibir();
         }
     }
 }
--- Montadora/Models/Carro.cs
+++ Montadora/Models/Carro.cs
@@ -1,14 +1,16 @@
-﻿namespace Models
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Models
 {
     public class Carro
     {
         public string? Modelo { get; set; }
         public string? Marca { get; set; }
         public int PercentualCombustivel { get; set; }
-        public string  Placa { get; set; }
+        public string Placa { get; set; }
         public int Ano { get; set; }
         public int VelocidadeAtual { get; set; }
-        public int VelocidadeMaxima  { get; set; }
+        public int VelocidadeMaxima { get; set; }
         public bool Ligado { get; set; }
         public int Odometro { get; set; }
         public Pneu PneuDianteiroEsquerdo { get; set; }
@@ -31,46 +33,134 @@
             PercentualCombustivel = 0;
             VelocidadeAtual = 0;
 
-            PneuDianteiroEsquerdo = new Pneu(16,"Carro de passeio", "Firestone", true, 150);
-            PneuDianteiroDireito = new Pneu(16, "Carro de passeio", "Firestone", true, 150);
-            PneuTraseiroEsquerdo = new Pneu(16, "Carro de passeio", "Firestone", true, 150);
-            PneuTraseiroDireito = new Pneu(16, "Carro de passeio", "Firestone", true, 150);
-            PneuEstepe = new Pneu(16, "Carro de passeio", "Firestone", true, 70);
-        }   
+            PneuDianteiroEsquerdo = new Pneu(16, "Carro de passeio", "Firestone", false, 150);
+            PneuDianteiroDireito = new Pneu(16, "Carro de passeio", "Firestone", false, 150);
+            PneuTraseiroEsquerdo = new Pneu(16, "Carro de passeio", "Firestone", false, 150);
+            PneuTraseiroDireito = new Pneu(16, "Carro de passeio", "Firestone", false, 150);
+            PneuEstepe = new Pneu(16, "Pneu de Estepe", "Firestone", true, 70);
+        }
 
         public void Ligar()
         {
-                if(PercentualCombustivel > 0)
+
+            if (PercentualCombustivel > 0)
+            {
+                Ligado = true;
+                PercentualCombustivel = PercentualCombustivel - 3;
+
+                if (PercentualCombustivel <= 0)
                 {
-                    Ligado = true;
+                    PercentualCombustivel = 0;
+                    Desligar();
                 }
+            }
         }
 
         public void Desligar()
         {
             Ligado = false;
+            Parar();
+        }
+
+        public void Acelerar(int _impulso)
+        {
+            if (Ligado == true && _impulso > 0)
+            {
+                Odometro += 18;
+
+                PercentualCombustivel = PercentualCombustivel - 8;
+                if (PercentualCombustivel <= 0)
+                {
+                    PercentualCombustivel = 0;
+                    Desligar();
+                    return;
+                }
+
+                VelocidadeAtual = VelocidadeAtual + _impulso;
+                PneuDianteiroDireito.Girar(_impulso);
+                PneuDianteiroEsquerdo.Girar(_impulso);
+                PneuTraseiroDireito.Girar(_impulso);
+                PneuTraseiroEsquerdo.Girar(_impulso);
+
+                if (PneuDianteiroDireito.Estourado || PneuDianteiroEsquerdo.Estourado || PneuTraseiroDireito.Estourado || PneuTraseiroEsquerdo.Estourado)
+                {
+                    Parar();
+                }
+            }
+
+        }
+
+        private void Parar()
+        {
             VelocidadeAtual = 0;
-            PneuDianteiroDireito.VelocidadeAtual= 0;
-            PneuDianteiroEsquerdo.VelocidadeAtual= 0;
+            PneuDianteiroDireito.VelocidadeAtual = 0;
+            PneuDianteiroEsquerdo.VelocidadeAtual = 0;
             PneuTraseiroDireito.VelocidadeAtual = 0;
             PneuTraseiroDireito.VelocidadeAtual = 0;
         }
 
-        public void Acelerar()
+        public void Frear(int _reduzir)
         {
+            VelocidadeAtual = VelocidadeAtual - _reduzir;
 
+            if (VelocidadeAtual < 0)
+            {
+                VelocidadeAtual = 0;
+            }
+
+            PneuDianteiroDireito.Frear(_reduzir);
+            PneuDianteiroEsquerdo.Frear(_reduzir);
+            PneuTraseiroDireito.Frear(_reduzir);
+            PneuTraseiroEsquerdo.Frear(_reduzir);
         }
 
-        public void Frear()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_quantidadeCombustivel">Informe o percentual de combustível que deseja abastercer, caso informe 0 o método irá completar o tanque. O valor não pode ser superior a 100.</param>
+        public void Abastecer(int _quantidadeCombustivel = 0)
         {
+            if (_quantidadeCombustivel == 0)
+            {
+                _quantidadeCombustivel = 100 - _quantidadeCombustivel;
+            }
 
+            if (PercentualCombustivel + _quantidadeCombustivel > 100)
+            {
+                Console.WriteLine("Quantidade de combustível ultrapassa o limite do tanque!");
+                return;
+            }
+
+            if (PercentualCombustivel < 100)
+            {
+                PercentualCombustivel = PercentualCombustivel + _quantidadeCombustivel;
+            }
         }
 
-        public void Abastercer()
+        public void Exibir()
         {
+            Console.WriteLine("Modelo: " + Modelo);
+            Console.WriteLine("Marca: " + Marca);
+            Console.WriteLine("Placa: " + Placa);
+            Console.WriteLine("Ano: " + Ano);
+            Console.WriteLine("Velocidade Máxima: " + VelocidadeMaxima);
+            Console.WriteLine("Ligado: " + Ligado);
+            Console.WriteLine("Odometro: " + Odometro);
+            Console.WriteLine("Percentual de Combustivel: " + PercentualCombustivel);
+            Console.WriteLine("Velocidade Atual: " + VelocidadeAtual);
+
+            Console.WriteLine("\nPneu Dianteiro Esquerdo");
+            PneuDianteiroEsquerdo.Exibir();
+            Console.WriteLine("\nPneu Dianteiro Direito");
+            PneuDianteiroDireito.Exibir();
+            Console.WriteLine("\nPneu Traseiro Esquerdo");
+            PneuTraseiroEsquerdo.Exibir();
+            Console.WriteLine("\nPneu Traseiro Direito");
+            PneuTraseiroDireito.Exibir();
+            Console.WriteLine("\nPneu Estepe");
+            PneuEstepe.Exibir();
 
         }
 
     }
 }
-    
